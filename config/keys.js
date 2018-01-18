726d93e2131832c45457a60de859c19eb608c179
YervantB <yebastikian@gmail.com>
2018-01-18 10:37:42 -0800

    initial repo

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: "699771158904-a7dei6a5t91td1omcreoobe7lq6emqhj.apps.googleusercontent.com",
+    googleClientSecret: "kIdkhk3zU591kHIJw-V9BcHP"
+};
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+require('./services/passport');
+
+
 const app = express();
 
-app.get('/', (req,res) => {
-    console.log(req.url + ":" + req.method);
-    res.send({hi: 'BR ES NUMERO UNO HUEHUEHUEHUEHUEHUEHUEHUE'});
-} );
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
