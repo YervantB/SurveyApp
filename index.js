511b986e6dba2dae7762694a0496af8666484e2f
YervantB <yebastikian@gmail.com>
2018-01-18 21:41:20 -0800

    fixed routing typo for heroku deployment

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app); //handle routs to google oauth
+require('./routes/authroutes')(app); //handle routs to google oauth
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
